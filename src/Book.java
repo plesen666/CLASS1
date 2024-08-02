c38c6b5c6a887a3f190a697b2bbd0f7982042f49
plesen666 <diada.var@mail.ru>
2024-08-02 23:20:16 +0300

    3

--- src/Book.java
+++ src/Book.java
@@ -1,22 +1,23 @@
 public class Book {
-    Author author;//тип данных состоящий из String firstName, String lastName
-    String titleName;
-    int publicationYear;
+    private Author author;//тип данных состоящий из String firstName, String lastName
+    private String titleName;
+    private int publicationYear;
 
     public Book(Author author, String titleName, int publicationYear) {
         this.author = author;
         this.titleName = titleName;
         this.publicationYear = publicationYear;
     }
+
     public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
     }
-public String getTitleName() {
-    return this.titleName;
-} public Author getAuthor(){
+    public String getTitleName() {
+        return this.titleName;
+    }
+    public Author getAuthor() {
         return this.author;
     }
-
     public int getPublicationYear() {
         return this.publicationYear;
     }
